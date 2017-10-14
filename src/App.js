9615a44bf29379abb3085cecf031a52ea6697218
Eric Dan <ericdan@Erics-MacBook-Pro.local>
2017-10-14 11:36:48 -0400

    switch to google maps

--- src/App.js
+++ src/App.js
@@ -1,39 +1,55 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
-
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 import './App.css';
 
+let 
+MyMapComponent = withScriptjs(withGoogleMap((props) =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: 45, lng: -75}}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: 45, lng: -75 }} />}
+  </GoogleMap>
+));
+
+
 class App extends Component {
-  LeafletMap() {
-    const position = [51.505, -0.09];
+  // LeafletMap() {
+  //   const position = [43, -75];
+  //   return (
+  //     <Map center={position} zoom={13}>
+  //       <TileLayer
+  //         url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+  //         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  //       />
+  //       <Marker position={position}>
+  //         <Popup>
+  //           <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+  //         </Popup>
+  //       </Marker>
+  //     </Map>
+  //   );
+  // }
+  render() {
     const style = {
       width: 500,
       height: 500
     }
-    return (
-      <Map style={style} center={position} zoom={13}>
-        <TileLayer
-          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <Marker position={position}>
-          <Popup>
-            <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
-          </Popup>
-        </Marker>
-      </Map>
-    );
-  }
-  render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <div>
-          {this.LeafletMap()}
+        <div style={style} className='leafletContainer'>
+        <MyMapComponent
+          isMarkerShown
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
         </div>
       </div>
     );
